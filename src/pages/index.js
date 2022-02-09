4ff034afc3334c58761b705703332d2a492ac8a8
Claudenir Fonseca <claudenirmf@gmail.com>
2022-02-09 08:24:31 +0100

    Update landing page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,18 +25,21 @@ const IndexPage = () => (
       <div className="container">
         <div className="row">
           <div className="col-lg-8 mx-auto">
+            <p className="text-black text-justify">
+              EDOC 2022 is the 26<sup>th</sup> conference in the <Link to="http://edocconference.org/">EDOC series</Link> that provides a key forum for researchers and practitioners in the field of Enterprise Computing. EDOC addresses the full range of models, methodologies, and engineering technologies contributing to building and evolving intra- and inter-enterprise software systems. From 2022 on, EDOC will be spelled-out "Enterprise Design, Operations and Computing" to reflect the broad range of aspects of interest to the conference.
+            </p>
 
-          <p className="text-black text-justify">
-            The <b>26<sup>th</sup> Enterprise Design, Operations and Computing</b> conference (<b>EDOC 2022</b>) provides the key forum for researchers and practitioners in the field of enterprise computing. Since its conception in 1997, the <Link to="http://edocconference.org/">EDOC conference series</Link> brings together leading computer science researchers, IT decision-makers, enterprise architects, solution designers, and practitioners to discuss enterprise computing challenges, models, and solutions from the perspectives of academia, industry, and government.
-          </p>
+            <p className="text-black text-justify">
+              EDOC 2022 calls for contributions with a special emphasis on the theme of designing and operating "Flexible Enterprises". This year's theme reflects an ever changing world under the influence of disruptive events, trends and technologies, as well as the increasing role of artificial intelligence.
+            </p>
 
-          <p className="text-black text-justify">
-            The EDOC’s unique emphasis is on a holistic view on enterprise applications engineering and management, fostering integrated approaches that address and relate business models, business processes, business policies, people, and technology - within and across enterprises. This emphasis has established its credibility and longevity and made it the premier enterprise distributed computing conference for addressing the full range of models, methodologies, and engineering technologies involved in building and evolving effective enterprise systems.
-          </p>
-            
-          <p className="text-black text-justify">
-            Please subscribe <Link to='http://eepurl.com/cGLBwf'>here</Link> to receive updates from the conference and future EDOC events. Watch a teaser video and come back soon for more details!
-          </p>
+            <p className="text-black text-justify">
+              In this context, new concepts and approaches are required: (a) to address pressing challenges in domains like healthcare, advanced manufacturing, finance, environmental management and sustainable agriculture, supply chain management and others, while ensuring compliance with ethical and legal principles, as well as long-term interoperability and resilience of enterprise systems; and (b) to leverage technologies such as digital twins, machine learning, IoT, big data analytics, distributed ledgers/blockchain in trusted enterprise practices. A particular challenge of interest in this scenario is the combined use of data-driven approaches (including machine learning) and other AI techniques with established modeling approaches for Enterprise Computing (including Enterprise Architecture Modeling, Business Process Modeling, Model-Driven Enterprise Systems Engineering, Conceptual Modeling, etc.) towards "augmented" enterprise systems.
+            </p>
+              
+            <p className="text-black text-justify">
+              Please subscribe <Link to="http://eepurl.com/cGLBwf">here</Link> to receive updates from the conference and future EDOC events. Watch a teaser video and come back soon for more details!
+            </p>
           </div>
         </div>
       </div>
