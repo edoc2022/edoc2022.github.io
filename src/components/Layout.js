2fbe79d63276153b168e84316816634bd6a3de39
Tiago Sales <tgoprince@gmail.com>
2022-05-17 08:48:28 +0200

    Fix bootstrap build issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
-
-import '../assets/sass/grayscale.scss';
 
 class Layout extends Component {
   render() {
